3d88f0902b33f7e0d32d56cc3dab48aa50206e73
Michael Prattinger <mprattinger@outlook.com>
2025-11-08 00:43:16 +0100

    making it easier

--- src/Visitor.Web/Infrastructure/Communication/VisitHub.cs
+++ src/Visitor.Web/Infrastructure/Communication/VisitHub.cs
@@ -4,8 +4,4 @@ namespace Visitor.Web.Infrastructure.Communication;
 
 public class VisitHub : Hub
 {
-    public async Task Broadcast(string message)
-    {
-        await Clients.All.SendAsync("Broadcast", message);
-    }
 }
